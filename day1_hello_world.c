828c939da936a7c7f201859b4fcd1f83d850325e
Priyanshu Mishra <Priyanshumishra4037@gmail.com>
2026-02-18 23:45:16 +0530

    Add files via upload

---
+++ day1_hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>   // Header File
+
+// main() is the entry point of every C program
+int main()
+{
+    // Printing output on screen
+    printf("Hello, World!\n");
+
+    printf("My name is Priyanshu.\n");
+    printf("Starting my C Programming Journey ");
+
+    return 0;   // Program ended successfully
+}
