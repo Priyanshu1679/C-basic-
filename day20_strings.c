d0a84e449ec2392cdd07f5e73737c59879706ac8
Priyanshu Mishra <Priyanshumishra4037@gmail.com>
2026-03-09 23:26:45 +0530

    Add files via upload

---
+++ day20_strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[50];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Your string is: %s", str);
+
+    printf("Length of string: %lu\n", strlen(str));
+
+    return 0;
+}
